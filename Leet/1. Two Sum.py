e67cb7930a0a52054ce588fed1094f009dda024c
jashilko-hp <jashilko@gmail.com>
2023-07-26 23:54:11 +0300

    26/07

---
+++ Leet/1. Two Sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+if __name__ == "__main__":
+    nums = list(map(int, input().split(',')))
+    target = int(input())
+    s = Solution()
+    s.twoSum(nums, target)
